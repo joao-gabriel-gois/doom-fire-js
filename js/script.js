14a35cc21aa821de33b3158c9a8d10823e385fc9
stdnt-cl <stdnt-cl@mac.git>
2019-09-25 22:23:55 -0300

    Final Adjustment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 import Fire from './modules/fire.js';
-const fireCanvasWidth = Math.round((window.innerWidth / 3));
+const fireCanvasWidth = Math.round((window.innerWidth / 8  < 1010 ? window.innerHeight / 6: window.innerWidth / 8));
 const fire = new Fire(fireCanvasWidth, 40, 'fireCanvas');
 fire.init(false, true);
